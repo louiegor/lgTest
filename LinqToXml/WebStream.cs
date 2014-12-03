02f0d67ee66790ce9368af32b5ada776c5b403be
Louis Ng <kamchuenlouisng@gmail.com>
2014-12-03 16:48:49 -0500

    add web stream and xml parser

---
+++ LinqToXml/WebStream.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LinqToXml
+{
+    [TestFixture]
+    public class WebStream
+    {
+        [Test]
+        public void StreamTest()
+        {
+            string data = "";
+            const string urlAddress = "http://google.com";
+            var request = (HttpWebRequest) WebRequest.Create(urlAddress);
+            var response = (HttpWebResponse) request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+            Assert.IsNotNull(data);
+        }
+    }
+}
