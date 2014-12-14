d2f17eac6ca805b99a45f183d9c1cd491ab5b6b0
louiegor <kamchuenlouisng@gmail.com>
2014-12-14 11:41:54 -0500

    a working file watcher

--- FileMonitor/Program.cs
+++ FileMonitor/Program.cs
@@ -14,29 +14,20 @@ namespace FileMonitor
         }
 
         private static FileSystemWatcher watcher;
-
-        private const string Path = @"c:\CODE\lgTest\louieGor.txt";
+        private const string DirPath = @"c:\CODE\lgTest\";
 
         private static void Watch()
         {
-            //if the file doesn't exist, create one
-            if (!File.Exists(Path))
-            {
-                using (var f = File.CreateText(Path))
-                {
-
-                }
-            }
-
             watcher = new FileSystemWatcher
                           {
-                              Path = @"C:\code",
+                              Path = @"c:\CODE\lgTest\",
                               NotifyFilter = NotifyFilters.LastWrite
                                              | NotifyFilters.FileName | NotifyFilters.DirectoryName,
                               Filter = "*.*"
                           };
 
             watcher.Changed += OnChanged;
+            watcher.Created += OnChangedDirectory;
             watcher.EnableRaisingEvents = true;
         }
 
@@ -44,12 +35,49 @@ namespace FileMonitor
 
         private static void OnChanged(object source, FileSystemEventArgs e)
         {
+            var name = e.Name;
+            var fullPath = e.FullPath;
+            var line = FileReader(fullPath);
             //Workaround for firing twice: http://stackoverflow.com/a/3042963
-            var lastWriteTime = File.GetLastWriteTime(Path);
+            var lastWriteTime = File.GetLastWriteTime(e.FullPath);
             if (lastWriteTime != lastRead)
-                Console.WriteLine("Something created");
+            {
+                Console.WriteLine("{0} has been changed", name);
+                Console.WriteLine("{0}", line);
+                Console.WriteLine();
+            }
+            //Console.WriteLine("The files has been changed");
 
             lastRead = lastWriteTime;
+        }
+
+        private static void OnChangedDirectory(object source, FileSystemEventArgs e)
+        {
+            var name = e.Name;
+            var fullPath = e.FullPath;
+            var line = FileReader(fullPath);
+            //Workaround for firing twice: http://stackoverflow.com/a/3042963
+            var lastWriteTime = Directory.GetLastWriteTime(DirPath);
+            if (lastWriteTime != lastRead)
+            {
+                Console.WriteLine("{0} has been created",name);
+                Console.WriteLine("{0}",line);
+                Console.WriteLine();
+            }
+            lastRead = lastWriteTime;
+        }
+
+        public static string FileReader (string fileName)
+        {
+            // Read the file and display it line by line.
+            string line;
+
+            using (var sr = new StreamReader(fileName))
+            {
+                line = sr.ReadToEnd();
+            }
+            
+            return line;
         }
     }
 }
---
+++ FileMonitor/TestFileWatcher.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FileMonitor
+{
+    [TestFixture]
+    public class TestFileWatcher
+    {
+        Program fileWatcher = new Program();
+
+        private const string DirectoryPath = @"c:\CODE\lgTest\testDirectory";
+        private string filePath = DirectoryPath + "\\test.txt";
+
+        [Test]
+        public void Test()
+        {
+            //const string lines = "First line.\r\nSecond line.\r\nThird line.";
+            //if the directory path does not exist, create it
+            if (!Directory.Exists(DirectoryPath))
+            {
+                //DirectoryInfo
+                  DirectoryInfo  di = Directory.CreateDirectory(DirectoryPath);
+                Console.ReadLine();
+            }
+            
+            if (!File.Exists(filePath))
+            {
+                using (var f = File.CreateText(filePath))
+                {
+
+                }
+            }
+
+            //var file = new StreamWriter(filePath);
+
+            Watch();
+
+            for (var i=0; i<10; i++)
+            {
+                string lines = String.Format("line {0}\r\n",i);
+                var file = new StreamWriter(filePath);
+                Thread.Sleep(1500);
+                file.WriteLine(lines);
+                file.Flush();
+                file.Close();
+                //WriteStuffTofile(file);
+            }
+
+            Assert.IsNotNull(DirectoryPath);
+        }
+
+        private static FileSystemWatcher watcher;
+
+        public static void Watch()
+        {
+            watcher = new FileSystemWatcher
+            {
+                //Path = @"C:\Users\louiegor\Documents\Interactive Data\FormulaOutput",
+                Path = DirectoryPath,
+                NotifyFilter = NotifyFilters.LastWrite
+                               | NotifyFilters.FileName |
+                               NotifyFilters.DirectoryName,
+                Filter = "*.*"
+            };
+
+            watcher.Changed += OnChanged;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private static DateTime lastRead = DateTime.MinValue;
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            //Workaround for firing twice: http://stackoverflow.com/a/3042963
+            var lastWriteTime = File.GetLastWriteTime(DirectoryPath);
+            if (lastWriteTime != lastRead)
+                Console.WriteLine("Something created");
+
+            lastRead = lastWriteTime;
+        }
+    }
+}
